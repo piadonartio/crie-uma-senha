6c115e7b662af30ce459307908072718290da20f
rodrigo <140639687+piadonartio@users.noreply.github.com>
2025-09-15 09:28:39 -0300

    00

--- script.js
+++ script.js
@@ -52,6 +52,12 @@ function geraSenha (){
         let entropia = tamanhaSenha * Math.Log2(tamanhoAlfabeto) ;
         forcaSenha.classList.remove('fraca', 'media', 'forte');
         if (entropia > 57){
-            
+            forcaSenha.classList.add('forte');
+        } else if (entropia > 35 && entropia < 57){
+            forcaSenha.classList.add( 'media');
+        } else if (entropia < 35){
+         forcaSenha.classList.add ('fraca'); 
         }
+        const valorEntropia = document.querySelector('.entropia');
+        valorEntropia.textContent = "Uma computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + "dias para descobrir essa senha" ;
     }
